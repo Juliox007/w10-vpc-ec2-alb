79c57aade847d18bf4c5b138fbb1eb77ba6cf41b
julio <juliosaki69@gmail.com>
2024-07-29 16:12:44 -0400

    terraform w10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "utcbocket1" #replace with your bucket
+    bucket         = "my_bucket" #replace with your bucket
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable" # replace with your table
+    dynamodb_table = "my_table" # replace with your table
   }
 }
